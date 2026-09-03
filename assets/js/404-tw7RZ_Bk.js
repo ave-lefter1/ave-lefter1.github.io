b8b834afb06970ff1ce0fd38ae4cc9f1b2fcff5c
luweijun <luweijun@jfgenius.com>
2026-09-03 18:38:56 +0800

    deploy

---
+++ assets/js/404-tw7RZ_Bk.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-B3m2VdQL.js";import{o,e as r}from"./vendor-BLQtflq9.js";const c={};function t(n,_){return o(),r("div",null,"404")}const f=e(c,[["render",t]]);export{f as default};
